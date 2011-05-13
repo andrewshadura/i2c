d9d480a059b61ec10653b5b26f0c0bd6178ba1fa
Tim <parizene@gmail.com>
2011-05-13 17:48:48 +0300

    updated dirs

---
+++ test/src/radeon_i2c.cpp
@@ -0,0 +1,89 @@
+#include "i2c.h"
+
+#define GPIO_VGA_DDC            0x0060
+#define GPIO_DVI_DDC            0x0064
+#define GPIO_MONID              0x0068
+#define GPIO_CRT2_DDC           0x006c
+
+/* GPIO bit constants */
+#define GPIO_A_0				(1 <<  0)
+#define GPIO_A_1				(1 <<  1)
+#define GPIO_Y_0				(1 <<  8)
+#define GPIO_Y_1				(1 <<  9)
+#define GPIO_EN_0				(1 << 16)
+#define GPIO_EN_1				(1 << 17)
+#define GPIO_MASK_0				(1 << 24)
+#define GPIO_MASK_1				(1 << 25)
+#define VGA_DDC_DATA_OUTPUT		GPIO_A_0
+#define VGA_DDC_CLK_OUTPUT		GPIO_A_1
+#define VGA_DDC_DATA_INPUT		GPIO_Y_0
+#define VGA_DDC_CLK_INPUT		GPIO_Y_1
+#define VGA_DDC_DATA_OUT_EN		GPIO_EN_0
+#define VGA_DDC_CLK_OUT_EN		GPIO_EN_1
+
+#define OUTREG(reg, state) (*((PUINT32)(reg)) = state)
+#define INREG(reg) (*((PUINT32)(reg)))
+
+// mmio??
+
+static VOID radeon_setsda(struct i2c_struct *st, UINT32 state)
+{
+	UCHAR *mmio = st->mmio_start_virtual;
+	UINT32 val;
+
+	val = INREG(st->ddc_base) & ~(VGA_DDC_DATA_OUT_EN);
+	if (!state)
+		val |= VGA_DDC_DATA_OUT_EN;
+
+	OUTREG(st->ddc_base, val);
+	(void)INREG(st->ddc_base);
+}
+
+static VOID radeon_setscl(struct i2c_struct *st, UINT32 state)
+{
+	UCHAR *mmio = st->mmio_start_virtual;
+	UINT32 val;
+
+	val = INREG(st->ddc_base) & ~(VGA_DDC_CLK_OUT_EN);
+	if (!state)
+		val |= VGA_DDC_CLK_OUT_EN;
+
+	OUTREG(st->ddc_base, val);
+	(void)INREG(st->ddc_base);
+}
+
+static UINT32 radeon_getsda(struct i2c_struct *st)
+{
+	UCHAR *mmio = st->mmio_start_virtual;
+	UINT32 val;
+
+	val = INREG(st->ddc_base);
+
+	return (val & VGA_DDC_DATA_INPUT) ? 1 : 0;
+}
+
+static UINT32 radeon_getscl(struct i2c_struct *st)
+{
+	UCHAR *mmio = st->mmio_start_virtual;
+	UINT32 val;
+
+	val = INREG(st->ddc_base);
+
+	return (val & VGA_DDC_CLK_INPUT) ? 1 : 0;
+}
+
+static VOID radeon_init(struct i2c_struct *st)
+{
+	st->ddc_base = GPIO_CRT2_DDC;
+
+	st->setsda = radeon_setsda;
+	st->setscl = radeon_setscl;
+	st->getsda = radeon_getsda;
+	st->getscl = radeon_getscl;
+	st->udelay = 10;
+
+	/* Raise SCL and SDA */
+	st->setsda(st, 1);
+	st->setscl(st, 1);
+	Sleep(20);
+}
---
+++ test/src/test.cpp
@@ -0,0 +1,43 @@
+#include "i810_i2c.cpp"
+#include "radeon_i2c.cpp"
+#include "i2c_base.cpp"
+#include "../external/pmdll.h"
+
+int main()
+{
+	// get pci device MMIO_PHYS_ADDRESS & MMIO_SIZE
+	// using pciutils
+
+	DWORD MMIO_PHYS_ADDRESS = 0xFFA80000;	// Intel 82865G Graphics Controller
+	DWORD MMIO_SIZE = 512 * 1024;
+
+	if (LoadPhyMemDriver() == FALSE)
+	{
+		printf("load phymem.sys failed\n");
+		return 1;
+	}
+
+	struct i2c_struct *st = (struct i2c_struct *)calloc(1, sizeof(struct i2c_struct));
+
+	st->mmio_start_virtual = (UINT8 *)MapPhyMem(MMIO_PHYS_ADDRESS, MMIO_SIZE);
+	printf("mapped virtual address = 0x%08x\n", st->mmio_start_virtual);
+
+	// check type
+	i810_init(st);
+
+	PUCHAR edid = (PUCHAR)calloc(128, sizeof(UCHAR));
+	if (GetEdid(st, edid, 128))
+	{
+		printf("\nEDID:\n");
+		for (int i = 0; i < 128; i++)
+			printf("0x%02x ", edid[i]);
+	}
+
+	UnmapPhyMem(st->mmio_start_virtual, MMIO_SIZE);
+	UnloadPhyMemDriver();
+
+	printf("\n\nend");
+	getchar();
+
+	return 0;
+}
